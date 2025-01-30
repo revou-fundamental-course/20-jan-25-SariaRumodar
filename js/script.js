78b16f5d5410b0bf1ffc5b8a6ee0303c6e2a762f
SariaRumodar <rumodarsaria@gmail.com>
2025-01-30 21:42:42 +0900

    update css, html, js (end)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
 // ini file javaScript yang akan dijalankan pada halaman web
+function calculateBmi() {
+    let resultBmi = document.getElementById('result-bmi');
+    let jenisKelamin = document.getElementById('input-jenis-kelamin').value;
+
+    let inputBeratBadan = document.getElementById('input-berat-badan').value;
+    let inputTinggiBadan = document.getElementById('input-tinggi-badan').value;
+    let statusBmi = document.getElementById('status-bmi');
+    let description = document.getElementById('bmi-description'); // Elemen untuk deskripsi
+
+    if (inputBeratBadan != '' && inputTinggiBadan != '') {
+        let bmi = (inputBeratBadan / ((inputTinggiBadan / 100) ** 2)).toFixed(2);
+        resultBmi.textContent = bmi;
+
+        // Menentukan status berdasarkan BMI
+        let status = "";
+        let desc = "";
+
+        if (bmi < 18.5) {
+            status = "Kekurangan berat badan";
+            desc = "Anda berada dalam kategori kekurangan berat badan. Sebaiknya Anda meningkatkan asupan nutrisi dan berkonsultasi dengan ahli gizi untuk mencapai berat badan ideal.";
+        } else if (bmi >= 18.5 && bmi <= 24.9) {
+            status = "Normal (Ideal)";
+            desc = "Anda memiliki berat badan normal. Pertahankan pola makan sehat dan rutin berolahraga untuk menjaga kesehatan tubuh.";
+        } else if (bmi >= 25 && bmi <= 29.9) {
+            status = "Kelebihan berat badan";
+            desc = "Anda berada dalam kategori overweight atau berat badan berlebih. Cara terbaik untuk menurunkan berat badan adalah dengan mengatur pola makan yang sehat dan rutin berolahraga.";
+        } else {
+            status = "Kegemukan (Obesitas)";
+            desc = "Anda berada dalam kategori obesitas. Dianjurkan untuk segera berkonsultasi dengan dokter atau ahli gizi untuk mendapatkan saran terbaik dalam menurunkan berat badan.";
+        }
+
+        statusBmi.textContent = status;
+        description.textContent = desc;
+    } else {
+        alert('Tolong isi semua data.');
+    }
+}
+
